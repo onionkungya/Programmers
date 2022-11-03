09948d1e3016ee8841f839808dfa80cb644ae3a8
ponion <114641838+onionkungya@users.noreply.github.com>
2022-11-03 18:27:22 +0900

    Add files via upload

---
+++ Lv.1/나머지가 1이 되는수 찾기.py
@@ -0,0 +1,7 @@
+def solution(n):
+    for i in range(1, n):
+        if n%i == 1:
+            return i
+
+#https://school.programmers.co.kr/learn/courses/30/lessons/87389
+#Programmers, Level1, 나머지가 1이 되는수 찾기
