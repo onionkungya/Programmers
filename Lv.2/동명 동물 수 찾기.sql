db545d5b6c2bf5f90b662cfc9d69db1ef60714a9
ponion <114641838+onionkungya@users.noreply.github.com>
2022-11-04 01:35:40 +0900

    Delete Lv.2 directory

--- Lv.2/동명 동물 수 찾기.sql
+++
@@ -1,8 +0,0 @@
-SELECT NAME, COUNT(NAME) AS 'COUNT'
-FROM ANIMAL_INS
-GROUP BY NAME HAVING COUNT(NAME)>=2
-ORDER BY NAME;
-
-#https://school.programmers.co.kr/learn/courses/30/lessons/59041
-#Programmers, Lv2, 동명 동물 수 찾기
-#GROUP BY 절의 HAVING 조건을 이용하여 문제를 해결하였습니다.
